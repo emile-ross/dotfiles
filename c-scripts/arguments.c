9e40035368fdd352ad7fae912c23bea95f0d1a83
imawsumm <imawsumm@users.noreply.github.com>
2026-05-11 22:52:31 -0400

    refactor: fix comments to c95 style
    also fix the indenting

--- c-scripts/arguments.c
+++ c-scripts/arguments.c
@@ -2,102 +2,102 @@
 
 void cli_arg_missing(char *first_command, char *type_of_missing_arg, char *user_flag_t)
 {
-    // prints an error message if there isn't a package specified in the command
+    /* prints an error message if there isn't a package specified in the command */
     printf(BOLD_S ANSI_RED"%s: missing %s after -- '%s'\n"STYLE_END, first_command, type_of_missing_arg, user_flag_t);
 }
 
 int parse_arguments(int num_cmd_arguments, char *cmd_arg_v[])
 {
-    if (num_cmd_arguments > 1) // checks how many arguments were used
+    if (num_cmd_arguments > 1) /* checks how many arguments were used */
     {
-	// checks if the command was ran with the --noconfirm flag
+		/* checks if the command was ran with the --noconfirm flag */
 
-	if (strcmp(cmd_arg_v[1], "--noconfirm") == 0) 
-	{
-	    printf(BOLD_S"Proceeding with full install\n"STYLE_END);
-	    full_install('y', 'y');
-	}
-	else if (strcmp(cmd_arg_v[1], "-p") == 0 || strcmp(cmd_arg_v[1], "-P") == 0)
-	{
-	    if (num_cmd_arguments >= n_to_arg)
-	    {
-		for (int i = n_to_arg - 1; i < num_cmd_arguments; i++)
+		if (strcmp(cmd_arg_v[1], "--noconfirm") == 0) 
 		{
-		    install_package(parent, cmd_arg_v[i]); 
+		    printf(BOLD_S"Proceeding with full install\n"STYLE_END);
+		    full_install('y', 'y');
 		}
-	    }
-	    else
-	    {
-		// prints an error message if there isn't a package specified in the command
-		cli_arg_missing(cmd_arg_v[0], "package", cmd_arg_v[1]);
-		error_message(304);
-	    }
-	}
-	else if (strcmp(cmd_arg_v[1], "-c") == 0 || strcmp(cmd_arg_v[1], "-C") == 0)
-	{
-	    if (strcmp(cmd_arg_v[1], "-C") == 0)
-	    {
-		error_message(7);
-	    }
-	    cmd_arg_install(num_cmd_arguments, cmd_arg_v, 'Y', 'N');
-	}
-	else if (strcmp(cmd_arg_v[1], "-i") == 0 || strcmp(cmd_arg_v[1], "-I") == 0)
-	{
-	    // loops through the arguments in order to pass them one at a time
-	    if (strcmp(cmd_arg_v[1], "-I") == 0)
-	    {
-		error_message(7);
-	    }
-
-	    if (num_cmd_arguments >= n_to_arg)
-	    {
-		for (int i = n_to_arg - 1; i < num_cmd_arguments; i++)
+		else if (strcmp(cmd_arg_v[1], "-p") == 0 || strcmp(cmd_arg_v[1], "-P") == 0)
 		{
-		    // will print a short description for the package
-		    //config_description();
-		    config_name description_index = detect_config_name(cmd_arg_v[i]);
-
-		    if ((config_name)description_index > n_configs)
+		    if (num_cmd_arguments >= n_to_arg)
 		    {
-			error_message((error_code_e)CLI_ARGS_MISSING);
+				for (int i = n_to_arg - 1; i < num_cmd_arguments; i++)
+				{
+				    install_package(parent, cmd_arg_v[i]); 
+				}
 		    }
-		    else if ((config_name)description_index == 0)
+		    else
 		    {
-			error_message((error_code_e)CLI_UNKNOWN_PKG);
+				/* prints an error message if there isn't a package specified in the command */
+				cli_arg_missing(cmd_arg_v[0], "package", cmd_arg_v[1]);
+				error_message(304);
+		    }
+		}
+		else if (strcmp(cmd_arg_v[1], "-c") == 0 || strcmp(cmd_arg_v[1], "-C") == 0)
+		{
+		    if (strcmp(cmd_arg_v[1], "-C") == 0)
+		    {
+				error_message(7);
+		    }
+		    cmd_arg_install(num_cmd_arguments, cmd_arg_v, 'Y', 'N');
+		}
+		else if (strcmp(cmd_arg_v[1], "-i") == 0 || strcmp(cmd_arg_v[1], "-I") == 0)
+		{
+		    /* loops through the arguments in order to pass them one at a time */
+		    if (strcmp(cmd_arg_v[1], "-I") == 0)
+		    {
+				error_message(7);
 		    }
 
-		    char *description = description_arr[(config_name)description_index];
-		    printf("%s\n", description);
+		    if (num_cmd_arguments >= n_to_arg)
+		    {
+				for (int i = n_to_arg - 1; i < num_cmd_arguments; i++)
+				{
+				    /* will print a short description for the package
+				     *config_description(); */
+				    config_name description_index = detect_config_name(cmd_arg_v[i]);
+
+				    if ((config_name)description_index > n_configs)
+				    {
+					error_message((error_code_e)CLI_ARGS_MISSING);
+				    }
+				    else if ((config_name)description_index == 0)
+				    {
+					error_message((error_code_e)CLI_UNKNOWN_PKG);
+				    }
+
+				    char *description = description_arr[(config_name)description_index];
+				    printf("%s\n", description);
+				}
+		    }
+		    else
+		    {
+				/* prints an error message if there isn't a package specified in the command */
+				cli_arg_missing(cmd_arg_v[0], "package", cmd_arg_v[1]);
+				error_message(304);
+		    }
 		}
-	    }
-	    else
-	    {
-		// prints an error message if there isn't a package specified in the command
-		cli_arg_missing(cmd_arg_v[0], "package", cmd_arg_v[1]);
-		error_message(304);
-	    }
-	}
-	else if (strcmp(cmd_arg_v[1], "--help") == 0)
-	{
-	    printf(BOLD_S"Help menu\n"STYLE_END);
-	    printf("-c	    	[CONFIG NAME] \n");
-	    printf("	apply specified config \n");
-	    printf("-p	    	[PACKAGE] \n");
-	    printf("	install specified package \n");
-	    printf("-i	    	[CONFIG NAME] \n");
-	    printf("	print a short description of the package\n");
-	}
-	else
-	{
-	    // prints an error message if the argument is invalid
-	    printf(BOLD_S ANSI_RED"%s: invalid option -- '%s'\n"STYLE_END, cmd_arg_v[0], cmd_arg_v[1]);
-	    error_message(303);
-	}
+		else if (strcmp(cmd_arg_v[1], "--help") == 0)
+		{
+		    printf(BOLD_S"Help menu\n"STYLE_END);
+		    printf("-c	    	[CONFIG NAME] \n");
+		    printf("	apply specified config \n");
+		    printf("-p	    	[PACKAGE] \n");
+		    printf("	install specified package \n");
+		    printf("-i	    	[CONFIG NAME] \n");
+		    printf("	print a short description of the package\n");
+		}
+		else
+		{
+		    /* prints an error message if the argument is invalid */
+		    printf(BOLD_S ANSI_RED"%s: invalid option -- '%s'\n"STYLE_END, cmd_arg_v[0], cmd_arg_v[1]);
+		    error_message(303);
+		}
     }
     else
     {
-	// if no argument is found (most cases)
-	return 0;
+		/* if no argument is found (most cases) */
+		return 0;
     }
     exit(0);
 }
@@ -176,16 +176,16 @@ void cmd_arg_install(int num_cmd_arguments_t, char *cmd_arg_v_t[], char config_a
     {
 	for (int i = n_to_arg - 1; i < num_cmd_arguments_t; i++)
 	{
-	    // support for different operating systems might come in the future...
-	    // will print the arguments instead of executing the command (useless)
+	    /* support for different operating systems might come in the future...
+	     * will print the arguments instead of executing the command (useless) */
 	    argument_config_install(cmd_arg_v_t[i], config_archive_t, pkg_install_t);
 	}
     }
     else
     {
-	// prints an error message if there isn't any config name specified in the command
-	cli_arg_missing(cmd_arg_v_t[0], "config name", cmd_arg_v_t[1]);
-	error_message(304);
+		/* prints an error message if there isn't any config name specified in the command */
+		cli_arg_missing(cmd_arg_v_t[0], "config name", cmd_arg_v_t[1]);
+		error_message(304);
     }
 }
 
@@ -226,20 +226,20 @@ config_name detect_config_name(char *input)
     if (strcmp(input, "waybar") == 0) return wayb;
     if (strcmp(input, "zsh") == 0) return zshh;
 
-    // alternative names
-    // check for hyprland
+    /* alternative names
+     * check for hyprland */
     int i = 1;
     while (HYPR_ARG_NAME[i] != NULL)
     {
 		if (strcmp(input, HYPR_ARG_NAME[i]) == 0) return hypr;
 		i++;
     }
-    // check for nvim
+    /* check for nvim */
     i = 1;
     while (NVIM_ARG_NAME[i] != NULL)
     {
-	if (strcmp(input, NVIM_ARG_NAME[i]) == 0) return nvim;
-	i++;
+		if (strcmp(input, NVIM_ARG_NAME[i]) == 0) return nvim;
+		i++;
     }
 
     if (strcmp(input, "swaywm") == 0) return sway;
